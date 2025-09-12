23dcdaa13f8d1f093cbd6e571bd7cef98093a96a
Safe Gergis <safegerg@gmail.com>
2025-09-12 12:24:00 -0800

    mend

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
 int main() {
   auto window =
       sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-  window.setFramerateLimit(144);
+  window.setFramerateLimit(60);
 
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
@@ -11,8 +12,13 @@ int main() {
         window.close();
       }
     }
-
     window.clear();
+
+    // draw everything here
+
+    sf::CircleShape player(50.0);
+    player.setFillColor(sf::Color(150, 50, 250));
+    window.draw(player);
     window.display();
   }
 }
