a8eb3436371b94a187c43cffbd2b0aba7f96c1e2
Josh Holman <josh@jolman.me>
2025-09-12 12:52:27 -0700

    add nix support

---
+++ default.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+mkShell {
+  nativeBuildInputs = [
+    cmake
+    ninja
+    pkg-config
+    clang
+    sfml
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXfixes
+    xorg.libXdamage
+    udev
+    freetype
+    openal
+    libvorbis
+    libogg
+    flac
+    libglvnd
+  ];
+}
